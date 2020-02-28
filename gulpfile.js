4f7bb1d273af1824675320f189944f22f96f1fca
Daniel Sibitzky <dan@mobelux.com>
2020-02-28 09:01:31 -0500

    images for build

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,26 @@
-const babel = require('gulp-babel');
-const cleancss = require('gulp-clean-css');
-const concat = require('gulp-concat');
-const connect = require('gulp-connect');
-const gulp = require('gulp');
-const htmlmin = require('gulp-htmlmin');
-const sass = require('gulp-sass');
-const sourcemaps = require('gulp-sourcemaps');
-const uglify = require('gulp-uglify');
-const argv = require('yargs').argv;
+const babel = require('gulp-babel')
+const cleancss = require('gulp-clean-css')
+const concat = require('gulp-concat')
+const connect = require('gulp-connect')
+const gulp = require('gulp')
+const htmlmin = require('gulp-htmlmin')
+const sass = require('gulp-sass')
+const sourcemaps = require('gulp-sourcemaps')
+const uglify = require('gulp-uglify')
+const argv = require('yargs').argv
 
 
 gulp.task('default', () => {
-    gulp.start(['js', 'sass', 'html']);
+    gulp.start(['js', 'sass', 'html', 'img'])
 
     if (!argv.build) {
-        gulp.watch(['src/scss/*', 'public/*.html'], ['sass']);
-        gulp.watch(['src/js/*'], ['js']);
-        gulp.watch(['src/index.html'], ['html']);
+        gulp.watch(['src/scss/*', 'public/*.html'], ['sass'])
+        gulp.watch(['src/js/*'], ['js'])
+        gulp.watch(['src/index.html'], ['html'])
 
-        gulp.start(['preview']);
+        gulp.start(['preview'])
     }
-});
+})
 
 gulp.task('preview', () => {
     connect.server({
@@ -28,16 +28,16 @@ gulp.task('preview', () => {
         port: 8080,
         livereload: true,
         debug: true,
-    });
-});
+    })
+})
 
 gulp.task('js', () => {
     const babelCompiler = babel({
         presets: ['env'],
     }).on('error', (err) => {
-        console.log(`babel: ${err.message}`);
-        babelCompiler.end();
-    });
+        console.log(`babel: ${err.message}`)
+        babelCompiler.end()
+    })
 
     gulp.src('src/js/*.js')
         .pipe(concat('index.js'))
@@ -49,14 +49,14 @@ gulp.task('js', () => {
         }))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('public/assets'))
-        .pipe(connect.reload());
-});
+        .pipe(connect.reload())
+})
 
 gulp.task('sass', () => {
     const sassCompiler = sass({
         style: 'compressed',
         errLogToConsole: false
-    }).on('error', sass.logError);
+    }).on('error', sass.logError)
 
     gulp.src([
         './src/scss/index.scss',
@@ -67,17 +67,22 @@ gulp.task('sass', () => {
         .pipe(cleancss())
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('public/assets'))
-        .pipe(connect.reload());
-});
+        .pipe(connect.reload())
+})
 
 gulp.task('html', () => {
-    let vinyl = gulp.src('src/index.html');
+    let vinyl = gulp.src('src/index.html')
 
     if (argv.build) {
         vinyl = vinyl.pipe(htmlmin({
-            collapseWhitespace: true
-        }));
+            collapseWhitespace: true,
+        }))
     }
 
-    vinyl.pipe(gulp.dest('public'));
-});
+    vinyl.pipe(gulp.dest('public'))
+})
+
+gulp.task('img', () => {
+    gulp.src('src/img/*')
+        .pipe(gulp.dest('public/assets/'))
+})
