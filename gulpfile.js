a3d5b54ad97db00184ccedf6cc7b4ef6937a7447
Dan <daniel@sibitzky.com>
2020-10-30 21:00:58 -0400

    Cleanup, spacing

--- gulpfile.js
+++ gulpfile.js
@@ -1,87 +1,73 @@
-const babel = require('gulp-babel')
+const argv = require('yargs').argv
 const cleancss = require('gulp-clean-css')
 const concat = require('gulp-concat')
 const connect = require('gulp-connect')
 const gulp = require('gulp')
-const htmlmin = require('gulp-htmlmin')
 const sass = require('gulp-sass')
-const uglify = require('gulp-uglify')
-const argv = require('yargs').argv
-const inline = require('gulp-inline');
 
 
 gulp.task('default', () => {
-    gulp.start(['js', 'sass', 'html', 'img'])
+  gulp.start(['js', 'sass', 'html', 'img'])
 
-    if (!argv.build) {
-        gulp.watch(['src/scss/*', 'public/*.html'], ['sass'])
-        gulp.watch(['src/js/*'], ['js'])
-        gulp.watch(['src/index.html'], ['html'])
+  if (!argv.build) {
+    gulp.watch(['src/scss/*', 'public/*.html'], ['sass'])
+    gulp.watch(['src/js/*'], ['js'])
+    gulp.watch(['src/index.html'], ['html'])
 
-        gulp.start(['preview'])
-    }
+    gulp.start(['preview'])
+  }
 })
 
 gulp.task('preview', () => {
-    connect.server({
-        root: ['public'],
-        port: 8080,
-        livereload: true,
-        debug: true,
-    })
+  connect.server({
+    root: ['public'],
+    port: 8080,
+    livereload: true,
+    debug: true,
+  })
 })
 
 gulp.task('js', () => {
-    const babelCompiler = babel({
-        presets: ['env', {
-            sourceMaps: true,
-        }],
-    }).on('error', (err) => {
-        console.log(`babel: ${err.message}`)
-        babelCompiler.end()
-    })
+  // const babelCompiler = babel({
+    // presets: ['env', {
+      // sourceMaps: true,
+    // }],
+  // }).on('error', (err) => {
+    // console.log(`babel: ${err.message}`)
+    // babelCompiler.end()
+  // })
 
-    gulp.src('src/js/*.js')
-        .pipe(concat('index.js'))
-        .pipe(babelCompiler)
-        .pipe(uglify({
-            compress: true,
-            mangle: true,
-        }))
-        .pipe(gulp.dest('public/assets'))
-        .pipe(connect.reload())
+  gulp.src('src/js/*.js')
+    .pipe(concat('index.js'))
+    // .pipe(babelCompiler)
+    // .pipe(uglify({
+    //   compress: true,
+    //   mangle: true,
+    // }))
+    .pipe(gulp.dest('public/assets'))
+    .pipe(connect.reload())
 })
 
 gulp.task('sass', () => {
-    const sassCompiler = sass({
-        style: 'compressed',
-        errLogToConsole: false
-    }).on('error', sass.logError)
+  const sassCompiler = sass({
+    errLogToConsole: false
+  }).on('error', sass.logError)
 
-    gulp.src([
-        './src/scss/index.scss',
-        //'./src/scss/print.scss',
-    ])
-        .pipe(sassCompiler)
-        .pipe(cleancss())
-        .pipe(gulp.dest('public/assets'))
-        .pipe(connect.reload())
+  gulp.src([
+    './src/scss/index.scss',
+  ])
+    .pipe(sassCompiler)
+    .pipe(cleancss())
+    .pipe(gulp.dest('public/assets'))
+    .pipe(connect.reload())
 })
 
 gulp.task('html', () => {
-    gulp.src('src/index.html')
-        // .pipe(inline({
-        //     base: 'public/',
-        //     disabledTypes: ['img'],
-        // }))
-        .pipe(htmlmin({
-            collapseWhitespace: true,
-            removeComments: true,
-        }))
-        .pipe(gulp.dest('public'))
+  gulp.src('src/index.html')
+    .pipe(gulp.dest('public'))
 })
 
 gulp.task('img', () => {
-    gulp.src('src/img/*')
-        .pipe(gulp.dest('public/assets/'))
+  gulp.src('src/img/*')
+    .pipe(gulp.dest('public/assets/'))
 })
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 "use strict"
 
+
 function rangeRand(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
@@ -7,120 +8,149 @@ function rangeRand(min, max) {
 const group = document.querySelector('.photo svg')
 const pieces = group.querySelectorAll('g circle, g path')
 
-let rotator
-function rotatePieces() {
-    const piece = pieces[Math.floor(Math.random() * pieces.length)]
-    piece.classList.add('hide')
-    setTimeout(() => {
-        piece.classList.remove('hide')
-    }, rangeRand(1000, 5000))
-    rotator = setTimeout(rotatePieces, rangeRand(200, 1000))
+function rotateSVGPieces() {
+  const piece = pieces[Math.floor(Math.random() * pieces.length)]
+  piece.classList.add('hide')
+  setTimeout(() => {
+    piece.classList.remove('hide')
+  }, rangeRand(1000, 2000))
+  setTimeout(rotateSVGPieces, rangeRand(200, 1000))
 }
-rotatePieces()
+rotateSVGPieces()
+
 
 const container = document.querySelector('.me')
 const canvas = container.querySelector('canvas')
 const context = canvas.getContext('2d')
 let particles = []
 
-const defaults = {
-    shape: 0,
-    phase: () => {}, // white
-    // phase: (x) => x / 50, // rainbow
-    // phase: () => parseInt(Math.random() * 20), // random colors
-    // phase: () => parseInt(350),
-}
-
-const over = (e) => {
-    defaults['phase'] = () => Math.random() * 20
-}
-
-const out = (e) => {
-    defaults['phase'] = () => {}
-}
-
-// keep the canvas at the right size as the window changes
 const resize = () => canvas.width = container.clientWidth
 window.addEventListener('resize', resize)
 resize()
 
+const shapes = {
+    bar: (state) => {
+      // tie width to alpha
+      const width = (state.radius * 2) + (state.alpha * 10)
+      const height = 120
+      const x = state.x - (state.alpha * 2)
+      const y = state.y + 120
+
+      context.moveTo(x, y) // bottom left
+      context.lineTo(x + (width), y) // bottom right
+      context.lineTo(x + (width), y - height) // top right
+      context.lineTo(x, y - height) // top left
+    },
+
+    circle: (state) => {
+      // y - radius to spawn above the top border
+      context.arc(state.x, state.y + state.radius, state.radius, 0, 6.2832);
+    },
+
+    triangle: (state) => {
+      const len = state.radius * 3
+      const y = state.y + len // make sure it forms above the top border
+
+      context.moveTo(state.x, y) // left
+      context.lineTo(state.x + (len / 2), y - (len * 0.9)) // center
+      context.lineTo(state.x + len, y) // right
+    },
+}
+
+const phases = {
+  white: () => {},
+  rainbow: (x) => x / 50,
+  random: () => parseInt(Math.random() * 20)
+}
+
+const defaults = {
+  shape: 'bar',
+  phase: phases.white,
+}
+
 function color(state) {
-    if (state.phase) {
-        return parseInt(Math.sin(0.0314 * state.y + 2 + state.phase) * 127 + 128) +','+
-               parseInt(Math.sin(0.0314 * state.y + 0 + state.phase) * 127 + 128) +','+
-               parseInt(Math.sin(0.0314 * state.y + 4 + state.phase) * 127 + 128)
-    }
-    return state.color
+  if (state.phase) {
+    return (
+      parseInt(Math.sin(0.0314 * state.y + 2 + state.phase) * 127 + 128) +','+
+      parseInt(Math.sin(0.0314 * state.y + 0 + state.phase) * 127 + 128) +','+
+      parseInt(Math.sin(0.0314 * state.y + 4 + state.phase) * 127 + 128)
+    )
+  }
+  return state.color
 }
 
 function update(state) {
-    if (!state || state.alpha < 0) {
-        const radius = 0.05 + Math.random() * 5.3
-        const x = Math.floor(Math.random() * (canvas.width - (radius*2) - 3)) + radius
-
-        return {
-            x: x,
-            y: canvas.height + 120,
-            alpha: 0.8 + Math.random() * 0.3,
-            radius: radius,
-            velocity: Math.random(),
-            color: '255,255,255',
-            phase: defaults.phase(x),
-            shape: defaults.shape,
-        }
-    }
+  if (!state || state.alpha < 0) {
+    const radius = 0.05 + Math.random() * 5.3
+    const x = Math.floor(Math.random() * (canvas.width - (radius*2) - 3)) + radius
 
     return {
-        x: state.x,
-        y: state.y - state.velocity,
-        alpha: state.alpha - 0.025,
-        radius: state.radius,
-        velocity: state.velocity + 0.03,
-        color: color(state),
-        phase: state.phase,
-        shape: state.shape,
+      x: x,
+      y: canvas.height,
+      alpha: 0.8 + Math.random() * 0.3,
+      radius: radius,
+      velocity: Math.random(),
+      color: '255,255,255',
+      phase: defaults.phase(x),
+      shape: defaults.shape,
     }
+  }
+
+  return {
+    x: state.x,
+    y: state.y - state.velocity,
+    alpha: state.alpha - 0.02,
+    radius: state.radius,
+    velocity: state.velocity + 0.03,
+    color: color(state),
+    phase: state.phase,
+    shape: state.shape,
+    debug: state.debug,
+  }
 }
 
 function draw(state) {
-    context.beginPath()
+  context.beginPath()
 
-    // width is tied to alpha so it shrinks as time goes on
-    const width = (state.radius * 2) + (state.alpha * 10)
-    const height = 120
-    const adjusted = state.x - (state.alpha * 2)
+  shapes[state.shape](state)
 
-    context.moveTo(adjusted, state.y) // bottom left
-    context.lineTo(adjusted + (width), state.y) // bottom right
-    context.lineTo(adjusted + (width), state.y - height) // top right
-    context.lineTo(adjusted, state.y - height) // top left
-
-    context.fillStyle = 'rgba('+ state.color +','+ state.alpha +')'
-    context.fill()
+  context.fillStyle = 'rgba('+ state.color +','+ state.alpha +')'
+  context.fill()
 }
-
-let particleCount = 50
 
 // slowly add new particles otherwise
 // they appear in waves and it looks like shit
+const particleCount = 50
 function addParticles() {
-    if (particles.length < particleCount) {
-        particles.push(update())
-        setTimeout(addParticles, 10)
+  if (particles.length < particleCount) {
+    for(let i=0; i<10; i++) {
+      particles.push(update())
     }
+    setTimeout(addParticles, 500)
+  }
 }
 addParticles()
 
 function run() {
-    context.clearRect(0, 0, canvas.width, canvas.height)
-    for (let i = particles.length; i--;) {
-        draw(particles[i] = update(particles[i]))
-    }
-    requestAnimationFrame(run)
+  context.clearRect(0, 0, canvas.width, canvas.height)
+  for (let i = particles.length; i--;) {
+    draw(particles[i] = update(particles[i]))
+  }
+  requestAnimationFrame(run)
 }
 run()
 
+const over = (e) => {
+  defaults['phase'] = phases.random
+  defaults['shape'] = 'circle'
+}
+
+const out = (e) => {
+  defaults['phase'] = phases.white
+  defaults['shape'] = 'bar'
+}
+
 document.querySelectorAll('footer, canvas').forEach(ele => {
-    ele.addEventListener('mouseover', over)
-    ele.addEventListener('mouseout', out)
+  ele.addEventListener('mouseover', over)
+  ele.addEventListener('mouseout', out)
 })
